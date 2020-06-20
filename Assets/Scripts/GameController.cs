2267c3896399c377d58354e4e2a5267504f40381
CoolLikeGoose <niken54@mail.ru>
2020-06-20 14:29:20 +0300

    Init commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private GameObject ball;
+    [SerializeField] private GameObject paddle;
+
+    public static Vector2 topRight;
+    public static Vector2 bottomLeft;
+
+    private void Start()
+    {
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        bottomLeft = Camera.main.ScreenToWorldPoint(Vector2.zero);
+
+        Instantiate(ball);
+
+        Instantiate(ball, )
+    }
+}
