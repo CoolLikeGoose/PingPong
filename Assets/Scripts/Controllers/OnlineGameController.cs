842d8807d73938745c1b33284272d4cf28ca5fa9
CoolLikeGoose <niken54@mail.ru>
2020-07-01 14:21:56 +0300

    BugFixed - ball movement is not smooth

--- Assets/Scripts/Controllers/OnlineGameController.cs
+++ Assets/Scripts/Controllers/OnlineGameController.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class OnlineGameController : MonoBehaviour
+public class OnlineGameController : MonoBehaviour//, IPunObservable
 {
     public static OnlineGameController Instance { get; private set; }
 
@@ -11,6 +11,8 @@ public class OnlineGameController : MonoBehaviour
     public float paddleSpeed;
     public float ballSpeed;
 
+    private GameObject BallObj;
+
     private void Awake()
     {
         Instance = this;
@@ -18,7 +20,7 @@ public class OnlineGameController : MonoBehaviour
 
     private void Start()
     {
-        if (PhotonNetwork.IsMasterClient) { PhotonNetwork.Instantiate("BallMP", Vector3.zero, Quaternion.identity); }
+        if (PhotonNetwork.IsMasterClient) { BallObj =  PhotonNetwork.Instantiate("BallMP", Vector3.zero, Quaternion.identity); }
     }
 
     public void OnMenuBtn()
@@ -26,4 +28,18 @@ public class OnlineGameController : MonoBehaviour
         PhotonNetwork.LeaveRoom();
         SceneManager.LoadScene(0);
     }
+
+    /*
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext((Vector2)BallObj.transform.position);
+        }
+        else
+        {
+            BallObj.transform.position = (Vector2)stream.ReceiveNext();
+        }
+    }
+    */
 }
